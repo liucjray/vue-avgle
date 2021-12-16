6491cdb2d69da6fa5b67c2caf42eb34449df86d0
cj <cj@gmail.com>
2021-12-16 14:12:07 +0800

    first

---
+++ src/views/Avgle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="avgle">
+    <Avgle msg="Welcome to Your AV App"/>
+  </div>
+</template>
+
+<script>
+import Avgle from '@/components/Avgle.vue'
+
+export default {
+  name: 'Avgle',
+  components: {
+   Avgle
+  }
+}
+</script>
